6849c379bf351c1df476e60ef85a11997e37331f
Jane <94185643+Jane1702@users.noreply.github.com>
2024-08-29 15:37:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -27,23 +27,25 @@ form_container_html = '''
 <div style="background-color:#7be5ff; padding:20px; border-radius:10px; box-shadow:0px 0px 15px rgba(0, 0, 0, 0.2);">
 '''
 st.markdown(form_container_html, unsafe_allow_html=True)
-with st.container():
+with st.form(key='prediction_form'):
     cols = st.columns(3)
+    
     data = {
-            'Nombre enfants': cols[0].number_input('Nombre enfants', min_value=0, step=1),
-            'GP-Impôt sur le Revenu brut': cols[1].number_input('GP-Impôt sur le Revenu brut', value=0.0, format='%f'),
-            'GP-Revenus professionnels foyer': cols[2].number_input('GP-Revenus professionnels foyer', value=0.0, format='%f'),
-            'GP-Solde Flux': cols[0].number_input('GP-Solde Flux', value=0.0, format='%f'),
-            'GP-Patrimoine Professionnel': cols[1].number_input('GP-Patrimoine Professionnel', value=0.0, format='%f'),
-            'GP-Passif cumulé': cols[2].number_input('GP-Passif cumulé', value=0.0, format='%f'),
-            'GP-Patrimoine immobilier': cols[0].number_input('GP-Patrimoine immobilier', value=0.0, format='%f'),
-            'GP-Patrimoine financier': cols[1].number_input('GP-Patrimoine financier', value=0.0, format='%f'),
-            'GP-Solde actif&passif': cols[2].number_input('GP-Solde actif&passif', value=0.0, format='%f'),
-            'GP-Total actif brut': cols[0].number_input('GP-Total actif brut', value=0.0, format='%f'),
-            'Age': cols[1].number_input('Age', min_value=0, step=1)
-        }    
-        
+        'Nombre enfants': cols[0].number_input('Nombre enfants', min_value=0, step=1),
+        'GP-Impôt sur le Revenu brut': cols[1].number_input('GP-Impôt sur le Revenu brut', value=0.0, format='%f'),
+        'GP-Revenus professionnels foyer': cols[2].number_input('GP-Revenus professionnels foyer', value=0.0, format='%f'),
+        'GP-Solde Flux': cols[0].number_input('GP-Solde Flux', value=0.0, format='%f'),
+        'GP-Patrimoine Professionnel': cols[1].number_input('GP-Patrimoine Professionnel', value=0.0, format='%f'),
+        'GP-Passif cumulé': cols[2].number_input('GP-Passif cumulé', value=0.0, format='%f'),
+        'GP-Patrimoine immobilier': cols[0].number_input('GP-Patrimoine immobilier', value=0.0, format='%f'),
+        'GP-Patrimoine financier': cols[1].number_input('GP-Patrimoine financier', value=0.0, format='%f'),
+        'GP-Solde actif&passif': cols[2].number_input('GP-Solde actif&passif', value=0.0, format='%f'),
+        'GP-Total actif brut': cols[0].number_input('GP-Total actif brut', value=0.0, format='%f'),
+        'Age': cols[1].number_input('Age', min_value=0, step=1)
+    }
+
     submit_button = st.form_submit_button(label='Predict')
+
 st.markdown('</div>', unsafe_allow_html=True)
 
 if submit_button:
