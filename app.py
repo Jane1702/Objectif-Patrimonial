925b16cadff347ff0407b86d6c4b0d1530d31cb6
Jane <94185643+Jane1702@users.noreply.github.com>
2024-08-29 15:05:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import joblib
 import pandas as pd
 
-title_html = '<p style="font-family:sans-serif; color:white; font-size: 42px; text-align:center;">Patrimonial Objective Prediction</p>'
+title_html = '<p style="font-family:sans-serif; color:white; font-size: 42px; text-align:center;">Prédiction Objective du Patrimoine</p>'
 st.markdown(title_html, unsafe_allow_html=True)
 model = joblib.load('Objectif Patrimonial.pkl')
 
